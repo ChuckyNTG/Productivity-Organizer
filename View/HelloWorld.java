5a96dbf2062e6194d7b7d97c0163c9702ae42880
ChuckyNTG <cd1996@comcast.net>
2016-09-28 15:03:25 -0400

    testing javaFX

---
+++ View/HelloWorld.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Hello World extends Application
+{
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
+
+	
